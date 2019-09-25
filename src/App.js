90b4f494d452b6368b1eae5385520e5ab72a4759
onyeka mmakwe <notmaks@yahoo.com>
2019-09-25 01:13:02 +0100

    update header

--- src/App.js
+++ src/App.js
@@ -1,42 +1,52 @@
-import React, { Component } from 'react';
-import './App.css';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import React, { Component } from "react";
+import "./App.css";
+import { Button, Form, FormGroup, Label, Input, FormText } from "reactstrap";
 
-const marked = require('marked')
+const marked = require("marked");
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
-    this.state= {
-      text: ''
-    }
+    this.state = {
+      text: ""
+    };
   }
 
-  updateMarkdown = (event) =>{
+  updateMarkdown = event => {
     this.setState({
       text: event.target.value
-    })
-  }
+    });
+  };
   render() {
-    const { text } = this.state
+    const { text } = this.state;
     return (
       <div className="text-center container">
-        <h1>Issa Markdown Tin🔥🔥🔥</h1>
+        <h1>Issa Markdown 🔥🔥🔥</h1>
         <div className="App">
           <div className="section">
             <Form>
               <FormGroup>
                 <h1>Text Output</h1>
-                <Input type="textarea" name="text" id="editor" placeholder="Enter Markdown" rows="20"  onChange={this.updateMarkdown}/>
+                <Input
+                  type="textarea"
+                  name="text"
+                  id="editor"
+                  placeholder="Enter Markdown"
+                  rows="20"
+                  onChange={this.updateMarkdown}
+                />
               </FormGroup>
             </Form>
           </div>
           <div className="section">
             <h1>Markdown Output</h1>
-          
-            <div className="markdown-output" dangerouslySetInnerHTML = {{__html:marked(text)}}></div>
+
+            <div
+              className="markdown-output"
+              dangerouslySetInnerHTML={{ __html: marked(text) }}
+            ></div>
           </div>
-      </div>
+        </div>
       </div>
     );
   }
